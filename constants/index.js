d86e63ad3a69fef5c27c6077721d19cfe95c3fcb
BenjaminRiderelli <riderelli.ben@gmail.com>
2023-10-10 11:09:11 +0200

    finished work for now

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,6 @@ import images from "./images";
 import icons from "./icons";
 import { COLORS, FONT, SIZES, SHADOWS } from "./theme";
 
-const rapidApiKey = "32d7595069msh7c5156123a40accp103056jsne434f983fb65";
+const rapidApiKey = "";
 
 export { images, icons, COLORS, FONT, SIZES, SHADOWS, rapidApiKey };
